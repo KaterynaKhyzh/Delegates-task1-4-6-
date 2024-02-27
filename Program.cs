f55e5298537a9b90bb00be58d79ac56358fca7ba
Катя Хижняк <Katerunka1905@gmail.com>
2024-02-27 23:15:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegates1
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Input number of seconds:");
+                int t = int.Parse(Console.ReadLine());
+                Timer timer = new Timer(() =>
+                {
+                    Console.WriteLine($"This method is executed every {t} seconds.");
+                }, t);
+
+            }
+        }
+}
---
+++ Timer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates1
+{
+    internal class Timer
+    {
+        private Action action;
+
+        public Timer(Action action, int t)
+        {
+            Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    action.Invoke();
+                    Thread.Sleep(TimeSpan.FromSeconds(t));
+                }
+            });
+            thread.Start();
+        }
+
+    }
+}
